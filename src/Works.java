3b046688fc42980b0bc3f0b6a74b1b10c76b6e9b
TopCoder <heroalex2013@mail.ru>
2021-03-17 00:35:01 +0300

    Initial commit

---
+++ src/Works.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Works {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        // String result = number < 5 ? "число меньше 5" : number > 5 ? "число больше 5" : "число равно 5";
+        if (number < 5){
+            System.out.println(число меньше 5);
+        }
+        if (number > 5){
+            System.out.println(число больше 5);
+        }
+        else
+        {
+            System.out.println(число равно 5);
+
+        }
+    }
+}
